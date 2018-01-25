adb5bf499ed4e658207ad5bd76fc164fc84d3f5f
Bibek Gupta <bibek.gupta@okstate.edu>
2018-01-24 20:23:32 -0600

    CIS2013_Week02_Lab02

---
+++ example.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+ int room_length = 5;
+ int room_width = 10;
+ float room_radius = 10.0;
+ float room_circle_area = (room_radius*room_radius)*3.14;
+ bool keep_running = true;
+ char question = 'Y';
+ /* == equal
+    != not equal
+	> greater
+	< less 
+	>= greater than or equal
+	<= less than or equal
+	&& both left and rigth are true
+	|| left and right are not true
+ */
+ while (keep_running) {
+	 cout << "Enter your Length: ";
+	 cin >> room_length;
+
+	 cout << "Enter your Width: ";
+	 cin >> room_width;
+	 
+	 if (room_length > room_width) {
+
+		 cout << "Length is greater" << endl;
+	 }
+	 else {
+		 cout << "width is greater" << endl;
+	 }
+	 cout << "Run again (Y/N): ";
+	 cin >> question;
+	 if ((question != 'y') && (question != 'Y')) {
+		 keep_running = false;
+	 }
+
+ }
+
+ 
+
+ return 0;
+
+
+
+}
